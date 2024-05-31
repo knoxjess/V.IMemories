62d7f8014867e00eb24bea87fcf702bd7a1cafdf
Knoxjess <jessy1234592@gmail.com>
2024-05-31 14:37:10 -0500

    finnal edits

--- assets/js/main.js
+++ assets/js/main.js
@@ -227,23 +227,6 @@
     }
   });
 
-  /**
-   * Testimonials slider
-   */
-  new Swiper('.testimonials-slider', {
-    speed: 600,
-    loop: true,
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false
-    },
-    slidesPerView: 'auto',
-    pagination: {
-      el: '.swiper-pagination',
-      type: 'bullets',
-      clickable: true
-    }
-  });
 
   /**
    * Animation on scroll
